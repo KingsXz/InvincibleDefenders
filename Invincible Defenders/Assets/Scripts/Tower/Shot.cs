0f7e3767789cb14a4ebdb477292c9c9179691783
KingsXz <Leandro2001xz@gmail.com>
2022-05-24 12:48:08 +0100

    Update 5
    - Better Shots

--- Invincible Defenders/Assets/Scripts/Tower/Shot.cs
+++ Invincible Defenders/Assets/Scripts/Tower/Shot.cs
@@ -6,14 +6,16 @@ public class Shot : MonoBehaviour
 {
     GameObject target;
     Transform initialPos;
-    [SerializeField] float shotSpeed = 1.2f;
+    [SerializeField] float shotSpeed;
     float shotDamage;
     float interpolateAmount;
     Vector2 goToPos;
+    Vector2 pos1;
+    Vector2 pos2;
 
     void Start()
     {
-        
+        shotSpeed = 1.2f;
     }
 
     
@@ -24,8 +26,11 @@ public class Shot : MonoBehaviour
             goToPos = target.transform.position;
         }
         interpolateAmount = interpolateAmount + Time.deltaTime * shotSpeed;
-        transform.position = Vector2.Lerp(initialPos.position, goToPos, interpolateAmount);
-        if(interpolateAmount >= 1)
+        pos1 = Vector2.Lerp(initialPos.position, new Vector2((initialPos.position.x + goToPos.x) / 2, (initialPos.position.y + goToPos.y) / 2) + new Vector2(0,1), interpolateAmount);
+        pos2 = Vector2.Lerp(new Vector2((initialPos.position.x + goToPos.x) / 2, (initialPos.position.y + goToPos.y) / 2) + new Vector2(0, 1), goToPos, interpolateAmount);
+        transform.position = Vector2.Lerp(pos1, pos2, interpolateAmount);
+        transform.up = new Vector2(transform.position.x - pos2.x, transform.position.y - pos2.y);
+        if (interpolateAmount >= 1)
         {
             if(target != null)
             {
